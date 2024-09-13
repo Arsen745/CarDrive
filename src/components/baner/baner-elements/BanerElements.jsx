077a48c6c5bb716c8b9360d583944e5b450dc180
Arsen <arsenkerezbekov7@gmail.com>
2024-09-13 19:19:44 +0600

    baner-elements

---
+++ src/components/baner/baner-elements/BanerElements.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './BanerElements.css'
+import ElectricCar from '../../../assets/svg/ElecrticCar.svg'
+import Hybrid from '../../../assets/svg/mdi_gasoline.svg'
+
+
+const BanerElements = () => {
+  return (
+    <div className='baner-elements'>
+        <div className="electric-car">
+            <img src={ElectricCar} alt="" />
+            <h3>Electric car</h3>
+
+        </div>
+        <div className="start">
+            <div className="start2">
+                <h4>Start</h4>
+            </div>
+
+        </div>
+        <div className="hybrid">
+            <img src={Hybrid} alt="" />
+            <h3>Hybrid Electric</h3>
+
+        </div>
+    </div>
+  )
+}
+
+export default BanerElements
---
+++ src/components/baner/baner-text/BanerText.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BannerText.css'
+
+const BanerText = () => {
+    return (
+        <div>
+            <div className="text-banner">
+
+                <h1>Выбери свой автомобиль</h1>
+                <h5>Bishkek, Kyrgyzstan</h5>
+            </div>
+        </div>
+    )
+}
+
+export default BanerText
