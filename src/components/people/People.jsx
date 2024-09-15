884d374d9c26da069e38e1d17a97065491740768
Arsen <arsenkerezbekov7@gmail.com>
2024-09-15 16:07:43 +0600

    slider people

--- src/components/people/People.jsx
+++ src/components/people/People.jsx
@@ -1,14 +1,35 @@
-import Peoples from '../../components/people/peoples/Peoples'
-import { BottomPeople } from './bottomPeople/BottomPeople'
-import { TopPeoples } from './topPeople/TopPeoples'
-import './People.css'
-import { PeopleButtons } from './peopleButtons/PeopleButtons'
+import React, { useRef } from 'react';
+import Peoples from '../../components/people/peoples/Peoples';
+import { BottomPeople } from './bottomPeople/BottomPeople';
+import { TopPeoples } from './topPeople/TopPeoples';
+import './People.css';
+import { PeopleButtons } from './peopleButtons/PeopleButtons';
 
 export const People = () => {
+    const peopleLightRef = useRef(null); 
+
+    const scrollLeft = () => {
+        if (peopleLightRef.current) {
+            peopleLightRef.current.scrollBy({
+                left: -1221, 
+                behavior: 'smooth', 
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (peopleLightRef.current) {
+            peopleLightRef.current.scrollBy({
+                left: 1221, 
+                behavior: 'smooth', 
+            });
+        }
+    };
+
     return (
         <div>
             <Peoples />
-            <div className='people-light container'>
+            <div className='people-light container' ref={peopleLightRef}>
                 <div className='card-people'>
                     <TopPeoples />
                     <BottomPeople />
@@ -21,9 +42,32 @@ export const People = () => {
                     <TopPeoples />
                     <BottomPeople />
                 </div>
-
+                <div className='card-people'>
+                    <TopPeoples />
+                    <BottomPeople />
+                </div>
+                <div className='card-people'>
+                    <TopPeoples />
+                    <BottomPeople />
+                </div>
+                <div className='card-people'>
+                    <TopPeoples />
+                    <BottomPeople />
+                </div>
+                <div className='card-people'>
+                    <TopPeoples />
+                    <BottomPeople />
+                </div>
+                <div className='card-people'>
+                    <TopPeoples />
+                    <BottomPeople />
+                </div>
+                <div className='card-people'>
+                    <TopPeoples />
+                    <BottomPeople />
+                </div>
             </div>
-            <PeopleButtons />
+            <PeopleButtons scrollLeft={scrollLeft} scrollRight={scrollRight} />
         </div>
-    )
-}
+    );
+};
--- src/components/people/peopleButtons/PeopleButtons.jsx
+++ src/components/people/peopleButtons/PeopleButtons.jsx
@@ -1,17 +1,21 @@
-import leftbtn from '../../../assets/svg/arrow-left.svg'
-import rightbtn from '../../../assets/svg/arrow-right.svg'
-import './PeopleButtons.css'
+import leftbtn from '../../../assets/svg/arrow-left.svg';
+import rightbtn from '../../../assets/svg/arrow-right.svg';
+import './PeopleButtons.css';
 
-export const PeopleButtons = () => {
+export const PeopleButtons = ({ scrollLeft, scrollRight }) => {
     return (
         <div>
             <div className='btns'>
-                <button><div className='leftbtn'><img src={leftbtn} alt="" /></div></button>
-                <button><div className='rightbtn'><img src={rightbtn} alt="" /></div></button>
+                <button onClick={scrollLeft}>
+                    <div className='leftbtn'><img src={leftbtn} alt="Scroll left" /></div>
+                </button>
+                <button onClick={scrollRight}>
+                    <div className='rightbtn'><img src={rightbtn} alt="Scroll right" /></div>
+                </button>
             </div>
             <div className='btn-review'>
                 <button className='btn-rev'>Оставить отзыв</button>
             </div>
         </div>
-    )
-}
+    );
+};
