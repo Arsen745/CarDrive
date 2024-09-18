4da8a6479b170fa93808439e771460adfa1b161d
unknown <mrlanovbajaman@gmail.com>
2024-09-18 11:34:55 +0300

    сделал описание

---
+++ src/components/autoImgTwo/AutoImgTwo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./AutoImgTwo.css"
+import audiTwo from "../../assets/png/AUDI.png"
+const AutoImgTwo = () => {
+    return (
+        <div>
+            <div className="audi-car">
+                <img src={audiTwo} alt="audi car"/>
+            </div>
+        </div>
+    );
+};
+
+export default AutoImgTwo;
